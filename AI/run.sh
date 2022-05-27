7be1b5f9edde9ae78075f31266c8773c88edaaee
jwel <whdgnl1123@gmail.com>
2022-05-27 15:42:47 +0900

    Change directory structure

--- AI/run.sh
+++ AI/run.sh
@@ -1,2 +1,2 @@
-PYTHONPATH=src/chess-alpha-zero/src nohup python src/main.py $@ >> log.txt 2>> log.txt &
+PYTHONPATH=chess-alpha-zero/src nohup python src/app.py $@ >> log.txt 2>> log.txt &
 jobs -p 1
